4ef3ffa2d812e19afcd80218f4f22b876ee9e8cf
AaritShrama <2007aarit@gmail.com>
2026-08-13 20:12:07 +0530

    Added PostgreSQL Alembic migrations

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from backend.app.schemas.semantic_message import SemanticMessage
 from backend.app.websocket.handlers import router as websocket_router
+from sqlalchemy.orm import Session
+from backend.app.db.session import get_db
+from backend.app.models.event import Event
+
 
 app = FastAPI(
     title="Ditributed AI System Backend",
@@ -17,8 +21,25 @@ async def health_check():
     }
 
 @app.post("/events")
-async def recieve_event(message : SemanticMessage):
+async def recieve_event(message : SemanticMessage, db : Session = Depends(get_db)):
+    event = Event(
+        vehicle_id=message.vehicle_id,
+        event_type=message.event_type,
+        object_type=message.object_type,
+        confidence=message.confidence,
+        risk_level=message.risk_level,
+        latitude=message.position.latitude,
+        longitude=message.position.longitude,
+        description=message.description,
+        recommendation=message.recommendation,
+        timestamp=message.timestamp
+        )
+    db.add(event)
+    db.commit()
+    db.refresh(event)    
+    
     return {
         "status": "recieved",
+        "event_id": event.id ,
         "message": message
     }
